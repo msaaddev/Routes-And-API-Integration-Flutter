2309d0189753db49d37dec9786e237dd78a16fe8
msaaddev <mrsaadirfan@gmail.com>
2021-05-04 02:48:35 +0500

    📦 NEW: get API call

--- lib/ListRecord.dart
+++ lib/ListRecord.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:assignment6/AppDrawer.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ListRecord extends StatefulWidget {
   @override
@@ -7,6 +10,20 @@ class ListRecord extends StatefulWidget {
 }
 
 class NewListRecord extends State<ListRecord> {
+  var data = [];
+
+  Future fetchRecords() async {
+    final apiURL = Uri.parse('https://pcc.edu.pk/ws/list/hms_consumers.php');
+    var response = await http.get(apiURL);
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      return data;
+    } else {
+      print('fail');
+      print(response);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +34,202 @@ class NewListRecord extends State<ListRecord> {
         body: SingleChildScrollView(
           child: ConstrainedBox(
             constraints: BoxConstraints(),
-            child: Text('List Record'),
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Center(
+                      child: Card(
+                          margin: EdgeInsets.only(top: 30),
+                          child: Container(
+                            width: 350,
+                            padding: EdgeInsets.only(top: 40, bottom: 20),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'All Records',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'Raleway',
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  child: ElevatedButton(
+                                      child: Text('View Records'),
+                                      style: ElevatedButton.styleFrom(
+                                        onPrimary: Colors.white,
+                                        elevation: 5,
+                                      ),
+                                      onPressed: () async {
+                                        await fetchRecords();
+
+                                        print('data');
+                                        print(data[1]['lastName']);
+                                        return showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return CupertinoAlertDialog(
+                                                title: Text('Records Fetched',
+                                                    style: TextStyle(
+                                                      fontFamily: 'Raleway',
+                                                      fontSize: 24,
+                                                    )),
+                                                actions: [
+                                                  TextButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text('Ok'))
+                                                ],
+                                              );
+                                            });
+                                      }),
+                                )
+                              ],
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: data.length != 0
+                      ? data.map((e) {
+                          if (e['id'] != 1) {
+                            Center(
+                              child: Card(
+                                margin: EdgeInsets.only(top: 30, bottom: 30),
+                                child: Container(
+                                  width: 350,
+                                  padding: EdgeInsets.only(top: 40, bottom: 20),
+                                  child: Column(
+                                    children: [
+                                      // first name
+                                      Container(
+                                          padding: EdgeInsets.only(
+                                              top: 40, bottom: 10, left: 30),
+                                          child: Row(children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(right: 30),
+                                              child: Text(
+                                                'First Name',
+                                                style: TextStyle(
+                                                  fontFamily: 'Raleway',
+                                                  fontSize: 14,
+                                                  color: Colors.grey[600],
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(e['firstName']),
+                                            ),
+                                          ])),
+                                      // last name
+                                      Container(
+                                          padding: EdgeInsets.only(
+                                              top: 40, bottom: 10, left: 30),
+                                          child: Row(children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(right: 30),
+                                              child: Text(
+                                                'Last Name',
+                                                style: TextStyle(
+                                                  fontFamily: 'Raleway',
+                                                  fontSize: 14,
+                                                  color: Colors.grey[600],
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(e['lastName']),
+                                            ),
+                                          ])),
+                                      // gender
+                                      Container(
+                                          padding: EdgeInsets.only(
+                                              top: 40, bottom: 10, left: 30),
+                                          child: Row(children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(right: 30),
+                                              child: Text(
+                                                'Gender',
+                                                style: TextStyle(
+                                                  fontFamily: 'Raleway',
+                                                  fontSize: 14,
+                                                  color: Colors.grey[600],
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(e['gender']),
+                                            ),
+                                          ])),
+                                      // email
+                                      Container(
+                                          padding: EdgeInsets.only(
+                                              top: 40, bottom: 10, left: 30),
+                                          child: Row(children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(right: 30),
+                                              child: Text(
+                                                'Email',
+                                                style: TextStyle(
+                                                  fontFamily: 'Raleway',
+                                                  fontSize: 14,
+                                                  color: Colors.grey[600],
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(e['email']),
+                                            ),
+                                          ])),
+                                      // phone number
+                                      Container(
+                                          padding: EdgeInsets.only(
+                                              top: 40, bottom: 10, left: 30),
+                                          child: Row(children: [
+                                            Container(
+                                              padding:
+                                                  EdgeInsets.only(right: 30),
+                                              child: Text(
+                                                'Number',
+                                                style: TextStyle(
+                                                  fontFamily: 'Raleway',
+                                                  fontSize: 14,
+                                                  color: Colors.grey[600],
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              width: 200,
+                                              child: Text(e['phone']),
+                                            ),
+                                          ])),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          }
+                        }).toList()
+                      : [
+                          Container(
+                            child: Text('Hello'),
+                          )
+                        ],
+                )
+              ],
+            ),
           ),
         ),
       ),
