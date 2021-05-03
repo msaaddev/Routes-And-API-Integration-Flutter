3b024f4a08d50a398b4c115329b603d7dd8761e6
msaaddev <mrsaadirfan@gmail.com>
2021-05-03 21:15:50 +0500

    📦 NEW: routes

--- lib/AppDrawer.dart
+++ lib/AppDrawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:assignment6/main.dart';
+import 'package:assignment6/AddRecord.dart';
+import 'package:assignment6/ListRecord.dart';
 
 class DrawerList extends StatefulWidget {
   @override
@@ -6,59 +9,77 @@ class DrawerList extends StatefulWidget {
 }
 
 class NewDrawerList extends State<DrawerList> {
-  List<String> labels = [
-    'Today',
-    'Products',
-    'Direct Materials',
-    'Direct Labour',
-    'Overheads',
-    'Customers',
-    'Suppliers',
-    'Consultants',
-    'Help',
-    'Settings',
-    'Logout'
-  ];
-
-  List<IconData> icons = [
-    Icons.email,
-    Icons.inbox,
-    Icons.group,
-    Icons.local_offer,
-    Icons.local_offer,
-    Icons.group,
-    Icons.group,
-    Icons.group,
-    Icons.help_outline,
-    Icons.settings,
-    Icons.logout
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: double.maxFinite,
-        child: ListView.builder(
-            itemCount: labels.length,
-            itemBuilder: (context, index) {
-              return Column(children: <Widget>[
+    return Drawer(
+        child: ListView(
+      children: [
+        Container(
+          height: 180.0,
+          child: DrawerHeader(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
                 Container(
-                    child: index == 1 || index == 7
-                        ? Divider(
-                            thickness: 3,
-                          )
-                        : null),
-                ListTile(
-                  leading: Icon(icons[index], size: 20),
-                  title: Text(labels[index],
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  dense: true,
-                )
-              ]);
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.network(
+                      'https://i.imgur.com/rG1TxNQ.png',
+                      height: 80,
+                      width: 80,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.arrow_forward_ios),
+          title: Text('Home Screen',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 16,
+                color: Colors.grey[600],
+              )),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return MyApp();
             }));
+          },
+        ),
+        ListTile(
+            leading: Icon(Icons.arrow_forward_ios),
+            title: Text('Add Records',
+                style: TextStyle(
+                  fontFamily: 'Raleway',
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                )),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return AddRecord();
+              }));
+            }),
+        ListTile(
+          leading: Icon(Icons.arrow_forward_ios),
+          title: Text('List Records',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 16,
+                color: Colors.grey[600],
+              )),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return ListRecord();
+            }));
+          },
+        ),
+      ],
+    ));
   }
 }
